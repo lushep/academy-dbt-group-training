81b0e947fc2a6c0e57033b0b56b80dded5ac0eb2
Jelle Hilbrands <jelle.hilbrands@bngbank.nl>
2025-12-02 11:07:52 +0000

    Introduce int_order_payments

--- jaffle_shop/models/marts/fct_orders.sql
+++ jaffle_shop/models/marts/fct_orders.sql
@@ -6,14 +6,20 @@ payments as (
     select * from {{ ref("stg_stripe__payments")}}
 ),
 
+int_order_payments as (
+    select * from {{ ref("int_order_payments") }}
+),
+
 final as (
     select 
         order_id,
         customer_id,
-        sum(payment_amount) as amount
+        sum(payment_amount) as amount,
+        {{ dbt_utils.star(from=ref('int_order_payments'), except=["order_id"], quote_identifiers=False) }}
     from orders
     left join payments using (order_id)
-    group by order_id, customer_id
+    left join int_order_payments using (order_id)
+    group by order_id, customer_id, {{ dbt_utils.star(from=ref('int_order_payments'), except=["order_id"], quote_identifiers=False) }}
 )
 
 select * from final
