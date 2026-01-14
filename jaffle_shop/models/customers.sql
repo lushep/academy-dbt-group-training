3c02dfac8d85b545b239fcf04d81eb6dcbeb2890
Lucy Sheppard <lucy.sheppard@xebia.com>
2026-01-14 05:49:50 +0000

    Add project to code

--- jaffle_shop/models/customers.sql
+++ jaffle_shop/models/customers.sql
@@ -4,7 +4,7 @@ with customers as (
         id as customer_id,
         first_name,
         last_name
-    from jaffle_shop.customers
+    from  `sql-training-422508.jaffle_shop.customers`
 
 ),
 
@@ -16,7 +16,7 @@ orders as (
         order_date,
         status
 
-    from jaffle_shop.orders
+    from  `sql-training-422508.jaffle_shop.orders`
 
 ),
 
